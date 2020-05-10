fe17c2be390bbc09a998cc42f9e8dc8923712327
David Meir-Levy <davidmeirlevy@gmail.com>
2020-05-10 21:59:46 +0300

    model with mongoose

--- models/todo.js
+++ models/todo.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose');
 
-const TodoSchema = new mongoose.Schema({}, {strict: false});
+const TodoSchema = new mongoose.Schema({
+	content: {
+		type: String,
+		required: true
+	},
+	isDone: {
+		type: Boolean,
+		default: () => false
+	}
+});
 const Todo = mongoose.model('Todo', TodoSchema);
 
 module.exports = Todo;
--- services/todos-service.js
+++ services/todos-service.js
@@ -2,24 +2,23 @@ const mongoose = require('mongoose')
 const Todo = require('../models/todo');
 
 function getTodos() {
-	return Todo.collection.find({}).toArray()
+	return Todo.find({})
 }
 
 function addTodo(todo) {
-	return Todo.collection.insertOne(todo)
-		.then(({insertedId}) => {
-			return Todo.collection.findOne({_id: insertedId})
-		});
+	todo = new Todo(todo);
+	return todo.save();
 }
 
 function removeTodo(todoId) {
-	return Todo.collection.deleteOne({_id: mongoose.Types.ObjectId(todoId)});
+	return Todo.findByIdAndRemove(todoId);
 }
 
-function editTodo(todoId, todo = {}) {
-	const {content, isDone = false} = todo;
-	return Todo.collection.updateOne({_id: mongoose.Types.ObjectId(todoId)}, {$set: {content, isDone}})
-		.then(() => Todo.collection.findOne({_id: mongoose.Types.ObjectId(todoId)}))
+function editTodo(todoId, newData = {}) {
+	return Todo.findById(todoId).then(todo => {
+		Object.assign(todo, newData);
+		return todo.save();
+	})
 }
 
 module.exports = {
