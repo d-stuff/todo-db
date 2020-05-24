610cbc623c02ae4c9d790d8defc39b80cd84144b
David Meir-Levy <davidmeirlevy@gmail.com>
2020-05-24 20:26:24 +0300

    fic mongo env var

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/todos',
+	mongoUri: process.env.MONGODB_URI || 'mongodb://localhost:27017/todos',
 	port: process.env.PORT || 3000,
 	tokenSecret: process.env.TOKEN_SECRET || 'my-secret-key1234'
 }
